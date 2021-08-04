983ff90d97e95d5bfaaac6edbd770df2e5c642db
Hennie <2749126@naver.com>
2021-08-04 15:34:00 +0900

    Level1 - 연습 문제 (x만큼 간격이 있는 n개의 숫자)

---
+++ src/level1/Solution3.java
@@ -0,0 +1,21 @@
+package level1;
+/**
+ *  x만큼 간격이 있는 n개의 숫자
+ */
+public class Solution3 {
+    public static void main(String[] args) {
+        System.out.println(solution(2, 5));
+    }
+    public static long[] solution(int x, int n) {
+        long[] answer = {};
+
+        answer = new long[n];
+        answer[0] = x;
+
+        for (int i = 1; i < n; i++) {
+            answer[i] = answer[i - 1] + x;
+        }
+
+        return answer;
+    }
+}
