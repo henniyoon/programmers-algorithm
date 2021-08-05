c80b05e5240e5c04caa8a5c0df54e428d1a972bc
Hennie <2749126@naver.com>
2021-08-05 18:53:00 +0900

    Level1 - 연습 문제 (짝수와 홀수)
    삼항 연산자!
    조건문 ? "참일 때" : "거짓일 때"

---
+++ src/level1/Solution7.java
@@ -0,0 +1,19 @@
+package level1;
+/**
+ *  짝수와 홀수
+ */
+public class Solution7 {
+    class Solution {
+        public String solution(int num) {
+            String answer = "";
+            if (num % 2 == 0) {
+                answer = "Even";
+            } else {
+                answer = "Odd";
+            }
+            return answer;
+            // 삼항 연산자를 활용한 깔끔한 한 줄 풀이..!
+//            return num % 2 == 0 ? "Even" : "Odd";
+        }
+    }
+}
