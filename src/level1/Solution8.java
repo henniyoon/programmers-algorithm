7963c779334bc67b50c59b6b0cbe0b1ffac91cf2
Hennie <2749126@naver.com>
2021-08-06 20:05:00 +0900

    Level1 - 연습 문제 (하샤드 수)

---
+++ src/level1/Solution8.java
@@ -0,0 +1,20 @@
+package level1;
+/**
+ *  하샤드 수
+ */
+public class Solution8 {
+    public static void main(String[] args) {
+        System.out.println(solution(200));
+    }
+
+    static boolean solution(int x) {
+        boolean answer = true;
+        int sum = (x / 10000) + (x / 1000) + (x % 1000 / 100) + (x % 100 / 10) + (x % 10);
+        System.out.println("sum = " + sum);
+        if (x % sum != 0) {
+            answer = false;
+        }
+        return answer;
+
+    }
+}
